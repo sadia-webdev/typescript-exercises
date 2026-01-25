e3b33fa8268ffd7381f94d9c12cbd806abb4070a
sadiya959 <sacdamamoud@gmail.com>
2026-01-25 11:23:27 +0300

    compeled exercise 2

---
+++ exercise_2/index.js
@@ -0,0 +1,5 @@
+var names = ['sadia', 'aisha', 'ali',];
+var grades = [80, 50, 90, 30];
+var statuss = [false, true, false];
+var product = ['Phone', 'Laptop'];
+var locations = ['Laascanood', 10, 30];
---
+++ exercise_2/index.ts
@@ -0,0 +1,9 @@
+const names: Array<string> = ['sadia','aisha', 'ali',]
+const grades: number[] = [80,50,90,30]
+const statuss: boolean[] = [false, true, false]
+
+const product: string[] = ['Phone', 'Laptop']
+
+
+const locations: [string, number, number] = ['Laascanood', 10, 30]
+
